227810eaaeae79bf422adeee1f22b2d1d022261b
Hassan123833 <ma03484189920@gmail.com>
2026-05-02 09:13:37 +0500

    Change background color of file upload dropzone

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ dark_theme_css = """
 
   /* File Upload */
   [data-testid="stFileUploadDropzone"] {
-    background-color: var(--bg-main);
+    background-color: #080616;
     border: 2px dashed var(--border);
     color: var(--text-primary);
   }
